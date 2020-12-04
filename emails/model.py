7c94f44bc78a97548aa427e4fdc65da5b12e77f2
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2020-12-04 19:59:35 +0100

    space reduction between captions and figures

--- emails/model.py
+++ emails/model.py
@@ -89,7 +89,7 @@ class EmailTemplateParser:
         district = filters.get("district")
         fname = f"{self.folder}/{district}/{self.config.get('date')}/{indicator}/{image_file_name}.png"
         self.payload[image_cid] = fname
-        return item +"<br style=\"line-height:5px\">"
+        return item +"<br style=\"line-height:1px\">"
 
     def __parse_image_title(self, item, filters):
         try:
