4293ca84ca9266df790e96959e3b8a7357a18fe9
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2020-12-04 12:42:27 +0100

    treemap deleted, aggs for country added

--- extract/model/figure_factory.py
+++ extract/model/figure_factory.py
@@ -2,6 +2,7 @@ from extract.dataset import helper
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 class FigureFactory:
@@ -64,86 +65,6 @@ class FigureFactory:
 
         return fig
 
-    def get_treemap(self, figure_object, data, figure_colors, **kwargs):
-
-        fig = go.Figure()
-        # We can add types here
-        fig_type = getattr(go, figure_object)
-
-        # figure_colors = self.colors.get("fig")
-
-        for name, df in data.items():
-
-            treemap_df = self.build_hierarchical_dataframe(
-                df.reset_index(),
-                levels=df.index.names,
-                value_column=df.columns[0],
-                total_id=name,
-            )
-
-            scale_max = round(
-                df[df.columns[0]].max() / df[df.columns[0]].sum() * 100, 2
-            )
-
-            fig.add_trace(
-                fig_type(
-                    parents=treemap_df["parent"],
-                    labels=treemap_df["id"],
-                    values=treemap_df["value"],
-                    branchvalues="total",
-                    marker=dict(
-                        colors=treemap_df["color"],
-                        colorscale=figure_colors,
-                        cmin=0,
-                        cmid=scale_max / 3,
-                        cmax=scale_max,
-                    ),
-                    textinfo="label+value+percent parent",
-                )
-            )
-            fig.update_layout(
-                margin=dict(t=20, b=20, r=20, l=20),
-                width=900,
-                height=400,
-                autosize=False,
-            )
-
-        return fig
-
-    # HELPER - TREEMAP
-
-    def build_hierarchical_dataframe(self, df, levels, value_column, total_id="total"):
-        levels = levels[::-1]
-        df_all_trees = pd.DataFrame(columns=["id", "parent", "value", "color"])
-        for i, level in enumerate(levels):
-            df_tree = pd.DataFrame(columns=["id", "parent", "value", "color"])
-            dfg = df.groupby(levels[i:]).sum()
-            dfg = dfg[dfg[dfg.columns[0]] > 0]
-            dfg = dfg.reset_index()
-            df_tree["id"] = dfg[level].copy()
-            if i < len(levels) - 1:
-                df_tree["parent"] = dfg[levels[i + 1]].copy()
-            else:
-                df_tree["parent"] = total_id
-            df_tree["value"] = dfg[value_column]
-            df_tree["color"] = dfg[value_column]
-            df_all_trees = df_all_trees.append(df_tree, ignore_index=True)
-        total = pd.Series(
-            dict(
-                id=total_id,
-                parent="",
-                value=df[value_column].sum(),
-                color=df[value_column].sum(),
-            )
-        )
-        df_all_trees = df_all_trees.append(total, ignore_index=True)
-        try:
-            df_all_trees["color"] = (
-                df_all_trees["color"] / df_all_trees["color"].max() * 100
-            )
-        except ZeroDivisionError as e:
-            print(e)
-        return df_all_trees
 
     #########
     # STYLE #
@@ -201,7 +122,12 @@ class FigureFactory:
                 facility_name = data.sort_values(by=indicator, ascending=False).reset_index().facility_name.iloc[0]
                 facility_value = data.loc[data.facility_name == facility_name, indicator].item()
                 parsed = str(round(facility_value/data[indicator].sum(),2) * 100) + "%"
-                print(parsed)
+            elif agg=="ratio_country":
+                parsed = self.__get_percentage_difference_between_time_country(db.datasets.get("country"))
+            elif agg=="reference_date":
+                data = db.datasets.get("country")
+                data_today= data.reset_index().date.max()
+                parsed = (data_today - relativedelta(years=1)).strftime("%B %Y")
             format_aggs.append(parsed)
         return title.format(*format_aggs)
 
@@ -245,3 +171,23 @@ class FigureFactory:
         reported_positive = str(data[data.date == max_date][indicator].count())
 
         return reported_positive
+
+
+    def __get_percentage_difference_between_time_country(self, data):
+        indicator = data.columns[-1]
+        data = data.reset_index()
+        max_date = data.date.max()
+        previous_year = (max_date - relativedelta(years=1))
+        print(previous_year)
+
+        ratio_value = round(
+            (
+                (data.loc[data.date == max_date, indicator].item() -
+                data.loc[data.date == previous_year, indicator].item()) /
+                data.loc[data.date == previous_year, indicator].item()
+            )
+            * 100
+        )
+        description = self.__get_percentage_description(ratio_value)
+
+        return description
