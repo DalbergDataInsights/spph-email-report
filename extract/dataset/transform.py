fe8cc660339787eb2094e6065e268b6991522346
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2020-12-08 16:59:42 +0100

    try/except for unavailable data

--- extract/dataset/transform.py
+++ extract/dataset/transform.py
@@ -42,16 +42,22 @@ def scatter_reporting_district_plot(data):
 def bar_district_plot(data):
 
     data_in = data.get("district_dated")
+    district= data_in.id.iloc[0]
     val_col = data_in.columns[-1]
     data_in = data_in.reset_index()
     data_in = data_in[data_in.date == data_in.date.max()]
-
     data_in = data_in[["facility_name", val_col]].groupby(by=["facility_name"]).sum()
+    #if all(value == 0 for value in data_in[val_col]): 
+        #print(f"No data available for this indicator")
+       # data_in = data_in.reset_index()
+        #data_in.loc[:,'facility_name'] =  district
+        #data_in=data_in.groupby('facility_name').sum()
+    #else:   
     data_in = data_in[data_in[val_col] > 0]
     data_in = data_in.sort_values(val_col, ascending = False).reset_index()
     data_in.loc[data_in.index >= 12, 'facility_name']='Others'
     data_in=data_in.groupby('facility_name').sum().sort_values(val_col)
     custom_dict = {"Others": 99}
-    data_in=data_in.sort_values(by=['facility_name'], key=lambda x: x.map(custom_dict))
+    data_in=data_in.sort_values(by=['facility_name'], key=lambda x: x.map(custom_dict))  
     return {"district": data_in}
  
