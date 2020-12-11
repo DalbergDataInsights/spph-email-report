6e6df3dbb86e7a5a79ab0adfa3e1803676909f60
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2020-12-11 17:44:07 +0100

    correcting pipeline for the new coverage caption

--- figures/pipeline.py
+++ figures/pipeline.py
@@ -23,70 +23,14 @@ national_pipeline = [
             2019: "rgb(106, 155, 195)",
             2020: "rgb(200, 19, 60)",
         },
-        "title": """Across the country, the {} {} between {} and {} """,
-        "title_args": [
-            "indicator_view",
-            "ratio_year",
-            "date_reference",
-            "date_national",
-        ],
-    },
-    {
-        "type": "bar",
-        "transform": bar_country_compare_plot,
-        "color": {
-            "Top/Bottom 10": "rgb(211, 41, 61)",
-        },
-        "args": {"bar_mode": "overlay"},
-        "title": "The {} {} between {} and {}",
-        "title_args": [
-            "indicator_view",
-            "ratio_year",
-            "date_reference",
-            "date_national",
-        ],
-    },
-    {
-        "type": "map",
-        "transform": map_country_compare_plot,
-        "color": {
-            "Change between reference and target date": [
-                "#b00d3b",
-                "#f77665",
-                "#dedad9",
-                "#96c0e0",
-                "#3c6792",
-            ],
-        },
-        "args": {
-            "bar_mode": "overlay",
-            "tolerance": 0.005,
-            "locations": "id",
-           # "gdf": gpd.read_file("./data/shapefiles/shapefile.shp"),
-        },
-        "title": "The {} {} between {} and {}",
-        "title_args": [
-            "indicator_view",
-            "ratio_year",
-            "date_reference",
-            "date_national",
-        ],
-    },
-    {
-        "type": "scatter",
-        "transform": reporting_count_transform,
-        "color": {
-            "Percentage of facilities expected to report which reported on their 105-1 form": "rgb(106, 155, 195)",
-            "Percentage of reporting facilities that reported a value of one or above for this indicator": "rgb(200, 19, 60)",
-        },
-        "title": "Reporting: on {}, {} of facilities reported on their 105:1 form, and, out of those, {} reported one or above for {}",
+        "title": """Across the country, in {} the {} is amounted <b>{}</b> """,
         "title_args": [
             "date_national",
-            "reporting_reported",
-            "reporting_positive",
             "indicator_view",
+            "latest_value",
+            
         ],
-    },
+    }
 ]
 
 
