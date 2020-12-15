dcb15062c903c2a4f60a68635f9df9bca9f1ec58
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2020-12-15 17:47:08 +0100

    draft! updated pipeline

--- figures/pipeline.py
+++ figures/pipeline.py
@@ -2,9 +2,7 @@ import dataset
 from dataset.transform import (
     scatter_district_plot,
     bar_district_plot,
-    scatter_reporting_district_plot,
-    scatter_country_plot,
-    reporting_district_count_transform
+    scatter_reporting_district_plot
 )
 
 from dataset.national_transform import (
@@ -15,7 +13,7 @@ from dataset.national_transform import (
 )
 #import geopandas as gpd
 
-national_pipeline = [ # this one is for titles for coverage! 
+national_pipeline = [ 
     {
         "type": "scatter",
         "transform": scatter_country_plot,
@@ -62,27 +60,8 @@ pipeline = [
         ],
     },
     {
-        "type": "bar",
-        "transform": scatter_reporting_district_plot,
-        "color": {
-            "Reported one or above for selected indicator": "rgb(42, 87, 131)",
-            "Reported null or zero for selected indicator": "rgb(247, 190, 178)",
-            "Did not report on their 105:1 form": "rgb(211, 41, 61)",
-        },
-        "args": {"bar_mode": "stack"},
-        # Of the 36 health facilities in Amuru, 16 health facilities reported with a number different from zero for the number of women attending their first ANC visit in September 2020
-        "title": "Of the {} health facilities expected to report in {}, <b>{} health facilities</b> reported with a number different from zero for the {} in {}",
-        "title_args": [
-            "facility_count",
-            "district",
-            "facility_count_reported",
-            "indicator_view",
-            "date",
-        ],
-    },
-    {
         "type": "scatter",
-        "transform": reporting_district_count_transform,
+        "transform": scatter_reporting_district_plot, 
         "color": {
             "Percentage of facilities expected to report which reported on their 105-1 form": "rgb(106, 155, 195)",
             "Percentage of reporting facilities that reported a value of one or above for this indicator": "rgb(200, 19, 60)",
@@ -90,8 +69,8 @@ pipeline = [
         "title": "Reporting: on {}, {} of facilities reported on their 105:1 form, and, out of those, {} reported one or above for {}",
         "title_args": [
             "date",
-            "facility_count",
-            "facility_count_reported",
+            "reporting_reported",
+            "reporting_positive",
             "indicator_view",
         ],
     },
