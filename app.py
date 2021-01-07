f4f131d9dcda8f0a6cbd1680d39b24f5432e6311
Valeriya Cherepova <cherepova.valeriya@gmail.com>
2021-01-07 17:30:08 +0100

    documenting the app

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ load_dotenv(find_dotenv())
 
 
 def run_extract(config, db, figure_pipeline):
-    # get the date
-    target_date = datetime.strptime(config.get("date"), "%Y%m")
+'''
+Fuction to create figures and captions for districs
+'''
+    target_date = datetime.strptime(config.get("date"), "%Y%m") # gets date from config.json
     print(f"Launching figure generation for {target_date}")
     reference_date = (target_date - timedelta(days=1)).replace(day=1)
 
     # for each district
-    # TODO separate district, date and indicator filter
-    # TODO filter by date
     for district in config.get("districts"):
         # TODO filter by district
         print(f"Running the pipeline of figures for {district}")
@@ -49,8 +49,10 @@ def run_extract(config, db, figure_pipeline):
 
 
 def run_extract_contry(config, db, figure_pipeline):
-    # get the date
-    target_date = datetime.strptime(config.get("date"), "%Y%m")
+'''
+Fuction to create figures and captions for the whole country
+'''
+    target_date = datetime.strptime(config.get("date"), "%Y%m") # gets date from config.json
     print(f"Launching figure generation for {target_date}")
     reference_date = target_date.replace(year=target_date.year - 1)
 
@@ -72,7 +74,9 @@ def run_extract_contry(config, db, figure_pipeline):
 
 
 def run_emails(config, engine, email_template, recipients):
-
+'''
+Fuction to parse a completed template and send it from a later defined email address to recipients
+'''    
     parser = EmailTemplateParser("data/viz", email_template, config)
 
     smtp = smtplib.SMTP(host=engine.get("smtp"), port=587)
@@ -87,12 +91,16 @@ def run_emails(config, engine, email_template, recipients):
 
 
 def run(pipeline):
+    '''
+    Main function, which calls the functions above using the customised configurations
+    '''
 
-    # Configs
-    DATABASE_URI = os.environ["HEROKU_POSTGRESQL_CYAN_URL"]
+    # Configurations: 
+    DATABASE_URI = os.environ["HEROKU_POSTGRESQL_CYAN_URL"] # sets the Database 
     config = get_config("config")
-    email_template = get_config("email_template")
-    recipients = get_config("email_recipients")
+    email_template = get_config("email_template") #sets the template
+    recipients = get_config("email_recipients") #sets the recipients
+    #sets sender's email incl. credentials 
     engine = {
         "smtp": os.environ["SMTP"],
         "username": os.environ["USERNAME"],
@@ -101,13 +109,13 @@ def run(pipeline):
 
     for pipe in pipeline:
 
-        if pipe == "extract":
-            db = Database(DATABASE_URI)
+        if pipe == "extract": #creates and prints images 
+            db = Database(DATABASE_URI) 
             pipeline = dataset.pipeline.get()
             db.init_pipeline(pipeline)
             run_extract(config, db, figures.pipeline)
 
-        elif pipe == "email":
+        elif pipe == "email": #compiles and sends emails 
             run_emails(config, engine, email_template, recipients)
 
         elif pipe == "extract_country":
